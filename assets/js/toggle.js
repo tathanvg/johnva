1a17e0746a3cb5e2658972c5e791286ee13ef0dd
tathanvg <tathanvg@icloud.com>
2026-01-12 04:13:06 +0000

    Add files via upload

---
+++ assets/js/toggle.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var element = document.getElementById("title");
+    element.classList.toggle("invert");
+    var element = document.getElementById("banner");
+    element.classList.toggle("invert");
+    var element = document.getElementById("spotlight");
+    element.classList.toggle("invert");
+    var element = document.getElementById("itech");
+    element.classList.toggle("invert");
+    var element = document.getElementById("design");
+    element.classList.toggle("invert");
+    var element = document.getElementById("certs");
+    element.classList.toggle("invert");
+    var element = document.getElementById("work");
+    element.classList.toggle("invert");
+    var element = document.getElementById("research");
+    element.classList.toggle("invert");
+    var element = document.getElementById("reports");
+    element.classList.toggle("invert");
+    var element = document.getElementById("skills");
+    element.classList.toggle("invert");
+    var element = document.getElementById("Footer");
+    element.classList.toggle("invert");
+  }
